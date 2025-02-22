441aaf725d4926c8c45049f960a588357af4b245
Souvik098 <86777300+Souvik098@users.noreply.github.com>
2025-02-22 16:51:16 +0530

    XM

--- commands/basic/bot.js
+++ commands/basic/bot.js
@@ -42,11 +42,7 @@ module.exports = {
         .setDescription(lang.inviteDescription)
     )
     // Subcommand: support
-    .addSubcommand(subcommand =>
-        subcommand
-            .setName('support')
-            .setDescription(lang.supportDescription)
-    ),
+    
   async execute(interaction) {
     if (interaction.isCommand && interaction.isCommand()) {
     const subcommand = interaction.options.getSubcommand();
@@ -62,33 +58,6 @@ module.exports = {
         .setTimestamp();
       
       await interaction.reply({ embeds: [embed] });
-    }
-    else if (subcommand === 'support') {
-        const supportServerLink = lang.supportServerLink;
-        const githubLink = lang.githubLink;
-        const replitLink = lang.replitLink;
-        const youtubeLink = lang.youtubeLink;
-
-        const embed = new EmbedBuilder()
-            .setColor('#b300ff')
-            .setAuthor({
-                name: lang.supportTitle,
-                iconURL: cmdIcons.rippleIcon,
-                url: supportServerLink
-            })
-            .setDescription(`
-                ➡️ **${lang.supportDescriptionTitle}:**
-                - ${lang.discord} - ${supportServerLink}
-                
-                ➡️ **${lang.followUsOn}:**
-                - ${lang.github} - ${githubLink}
-                - ${lang.replit} - ${replitLink}
-                - ${lang.youtube} - ${youtubeLink}
-            `)
-            .setImage(lang.supportImageURL)
-            .setTimestamp();
-
-        await interaction.reply({ embeds: [embed] });
     }
     else if (subcommand === 'invite') {
       const clientId = interaction.client.user.id;
